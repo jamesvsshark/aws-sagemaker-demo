f3eb04f0591125c6a695f31e331112ddcc790533
James Simoes <james@pocostud.io>
2018-11-02 10:47:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+  console.log("loaded", window);
+
+  var idToken =
+    "eyJraWQiOiJEWUZ2QkUwd0wxYWd5bWFIYm80SjJRUFdjZkJ0OUFTdk1cL0hpYTJPMVg1UT0iLCJhbGciOiJSUzI1NiJ9";
+
+  AWS.config.region = "us-east-2"; // Region
+  AWS.config.credentials = new window.AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "us-east-2:1ec48939-331f-4580-9214-dd727a340a40"
+  });
+
+  //   window.AWS.config.update({
+  //     region: "us-east-2",
+  //     credentials: new window.AWS.CognitoIdentityCredentials({
+  //       IdentityPoolId: "us-east-2:1ec48939-331f-4580-9214-dd727a340a40",
+  //       Logins: {
+  //         "cognito-idp.us-east-2.amazonaws.com/us-east-2_XYxeuAeSg": idToken
+  //       }
+  //     })
+  //   });
+
+//   window.AWS.config.getCredentials(function(credentials, error) {
+//     console.log(credentials, error);
+//   });
+
+  var sageMaker = new window.AWS.SageMaker();
+
+  sageMaker.createPresignedNotebookInstanceUrl(
+    { NotebookInstanceName: "notebook" },
+    function(err, data) {
+      console.log(err, data);
+    }
+  );
+};
